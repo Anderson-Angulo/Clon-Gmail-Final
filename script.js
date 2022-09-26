aed7a8bbf22f604a6809679f458de81c1ac49058
AndersonAngulo <andersonangulo1704@gmail.com>
2022-09-26 17:42:00 -0500

    Por terminar aun, 95%

---
+++ script.js
@@ -0,0 +1,22 @@
+// @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap");
+// :root {
+//   /* Colores Base */
+//   --accent-color: #ff0000;
+//   --grey-color: #909090;
+//   --grey-color-alt: #ccc;
+//   --border-color: rgba(0, 0, 0, 0.1);
+//   /* Colores Fondo */
+//   --bg-color: #fff;
+//   --bg-color-alt: #f9f9f9;
+//   --bg-color-active: #e5e5e5;
+//   /* Colores de texto */
+//   --text-color: #030303;
+//   --text-color-alt: #606060;
+//   --text-color-white: #fff;
+//   /* Texto */
+//   --body-font: "Roboto", sans-serif;
+// }
+
+// body {
+//   font-family: var(--body-font);
+// } 
